272137e2ccc242627f0268755dc8a65db12f0d8a
KysakaLoL <olegvasilev8171@gmail.com>
2026-04-28 02:09:59 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Раздаем статику из корня (или создай папку public, если хочешь порядка)
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
     console.log('Игрок подключился:', socket.id);
 
-    // Когда кто-то отгадал слово, сервер пересылает это ВСЕМ остальным
     socket.on('word_guessed', (data) => {
         socket.broadcast.emit('sync_word', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Игрок отключился:', socket.id);
     });
 });
 
